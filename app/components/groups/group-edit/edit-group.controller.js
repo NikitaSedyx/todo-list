6dd285bd3a85ac2ac93187aa98f711d12c97dcac
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-08-05 11:44:08 +0300

    add edit group controller

--- app/components/groups/group-edit/edit-group.controller.js
+++ app/components/groups/group-edit/edit-group.controller.js
@@ -4,8 +4,55 @@
 
     .controller("EditGroupController", EditGroupController)
 
-    EditGroupController.$inject = ["$scope"]
+    EditGroupController.$inject = ["GroupResource", "ItemResource",
+      "$scope", "$state", "$stateParams"]
 
-    function EditGroupController($scope){
+    function EditGroupController(GroupResource, ItemResource, $scope, $state, $stateParams){
+      getGroup()
+
+      $scope.newTask = {}
+
+      $scope.addTask = addTask
+      $scope.apply = apply
+      $scope.deleteGroup = deleteGroup
+      $scope.deleteTask = deleteTask
+      $scope.updateGroup = updateGroup
+
+      function addTask(){
+        $scope.group.items.push($scope.newTask)
+        $scope.newTask = {}
+        updateGroup()
+      }
+
+      function apply(){
+        updateGroup()
+        $state.go("groups.list")
+      }
+
+      function deleteGroup(){
+        $scope.group.is_deleted = true
+        apply()
+      }
+
+      function deleteTask(item){
+        ItemResource.deleteItem(item).$promise
+        .then(function(response){
+          getGroup()
+        })
+      }
+
+      function getGroup(){
+        GroupResource.getGroup({id: $stateParams.id}).$promise
+        .then(function(response){
+          $scope.group = response
+        })    
+      }
+
+      function updateGroup(){
+        GroupResource.editGroup($scope.group).$promise
+        .then(function(response){
+          getGroup()
+        })   
+      }
     }
 })()
