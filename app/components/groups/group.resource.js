ef0eaf7b93c843f51029ee8a7faa2952e0b169f4
Mikhail Mylitsa <atreidas1@gmail.com>
2015-08-05 10:32:36 +0300

    fixed

--- app/components/groups/group.resource.js
+++ app/components/groups/group.resource.js
@@ -1,25 +1,27 @@
 ;(function(){
   angular
     .module("todo")
+    .service("GroupResource",GroupResource)
 
-    .service("GroupResource",["$resource", "API", function($resource, API){
-      return $resource(API.BASE + API.GROUP + ":id/", {id: "@id"}, {
-        getGroups: {
-          method: "GET",
-          params: {id:null}
-        },
-        getGroup: {
-          method: "GET"
-        },
-        createGroup: {
-          method: "POST"
-        },
-        editGroup: {
-          method: "PUT"
-        },
-        deleteGroup: {
-          method: "DELETE"
-        }
-      })
-    }])
+  GroupResource.$inject=["$resource", "API"];
+
+  function GroupResource($resource, API) {
+    return $resource(API.BASE + API.GROUP + ":id/", {id: "@id"}, {
+      getGroups: {
+        method: "GET",
+        params: {id:null}
+      },
+      getGroup: {
+        method: "GET"
+      },
+      createGroup: {
+        method: "POST"
+      },
+      editGroup: {
+        method: "PUT"
+      },
+      deleteGroup: {
+        method: "DELETE"
+      }
+    })
 })()
