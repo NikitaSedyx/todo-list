c9c0061d98d0603a7a68f330c2aba581d4fbb993
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-08-06 14:23:55 +0300

    add editing contributors

--- app/components/groups/group-edit/edit-group.controller.js
+++ app/components/groups/group-edit/edit-group.controller.js
@@ -4,8 +4,36 @@
 
     .controller("EditGroupController", EditGroupController)
 
-    EditGroupController.$inject = ["$scope"]
+    EditGroupController.$inject = ["GroupResource", "$modal", "$scope",
+      "$state", "$stateParams"]
 
-    function EditGroupController($scope){
+    function EditGroupController(GroupResource, $modal, $scope, $state, $stateParams){
+      getGroup()
+
+      $scope.editContributors = editContributors
+
+      function editContributors(){
+        var contributorsModal = $modal.open({
+          templateUrl: "app/views/components/groups/group-edit/edit-contributors.html",
+          controller: "EditContributorsController",
+          resolve: {
+            contributors: function(){
+              return $scope.group.users
+            }
+          }
+        })
+        contributorsModal.result
+        .then(function(contributors){
+          $scope.group.users = contributors
+          updateGroup()
+        })
+      }
+
+      function getGroup(){
+        GroupResource.getGroup({id: $stateParams.id}).$promise
+        .then(function(response){
+          $scope.group = response
+        })    
+      }
     }
 })()
