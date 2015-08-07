462168bf7940637d219ce132fb5fb67ed35771ca
Mikhail Mylitsa <atreidas1@gmail.com>
2015-08-07 14:22:52 +0300

    changin view

--- app/components/groups/group.contoller.js
+++ app/components/groups/group.contoller.js
@@ -4,18 +4,20 @@
 
     .controller("GroupController", GroupController)
 
-    GroupController.$inject = ["GroupStorage", "$scope"]
+    GroupController.$inject = ["GroupStorage", "$scope", "SessionUserService"]
 
-    function GroupController(GroupStorage, $scope){
+    function GroupController(GroupStorage, $scope, SessionUserService){
       $scope.groupView = "list"
       $scope.groups = GroupStorage.groups
       $scope.params = {
         offset: 0
       }
       $scope.changeGroupView = changeGroupView
-      function changeGroupView(){
+      function changeGroupView(groupView){
+        SessionUserService.userView = groupView;
         $scope.params.offset = 0
         GroupStorage.groups.data = []
+
       }
     }
 })()
