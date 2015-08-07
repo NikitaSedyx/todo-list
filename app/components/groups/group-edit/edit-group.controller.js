477f73db1df158babd08fab457f53e39f451bac2
Nikita Sedyh <nikita.sedyx.94@gmail.com>
2015-08-07 10:09:42 +0300

    add resolve id and css class for modal window

--- app/components/groups/group-edit/edit-group.controller.js
+++ app/components/groups/group-edit/edit-group.controller.js
@@ -4,18 +4,19 @@
 
     .controller("EditGroupController", EditGroupController)
 
-    EditGroupController.$inject = ["GroupResource", "$modal", "$scope",
-      "$state", "$stateParams"]
+    EditGroupController.$inject = ["groupId", "GroupResource", "$modal", "$scope",
+      "$state"]
 
-    function EditGroupController(GroupResource, $modal, $scope, $state, $stateParams){
+    function EditGroupController(groupId, GroupResource, $modal, $scope, $state){
       getGroup()
 
       $scope.editContributors = editContributors
 
       function editContributors(){
         var contributorsModal = $modal.open({
-          templateUrl: "app/views/components/groups/group-edit/edit-contributors.html",
+          templateUrl: "app/views/components/groups/edit-contributors/edit-contributors.html",
           controller: "EditContributorsController",
+          windowClass: "edit-contributors-modal",
           resolve: {
             contributors: function(){
               return $scope.group.users
@@ -30,7 +31,7 @@
       }
 
       function getGroup(){
-        GroupResource.getGroup({id: $stateParams.id}).$promise
+        GroupResource.getGroup({id: groupId}).$promise
         .then(function(response){
           $scope.group = response
         })    
