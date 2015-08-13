05350a0d4906b4a7cd4542e220adaf2fcf0c6c13
Mikhail Mylitsa <atreidas1@gmail.com>
2015-08-13 15:26:24 +0300

    add login

---
+++ app/components/login/login.controller.js
@@ -0,0 +1,27 @@
+;(function () {
+  angular
+    .module("todo")
+    .controller("LoginController", LoginController);
+
+  LoginController.$inject = ["$scope", "$http", "$state", "API", "SessionUser"];
+
+  function LoginController($scope, $http, $state, API, SessionUser) {
+    $scope.signIn = signIn;
+    $scope.isMsgHide = true;
+
+    function signIn() {
+      $http.post(API.BASE + API.AUTH + API.LOGIN, $scope.user)
+        .then(loginSucces, loginError);
+    }
+
+    function loginSucces(response) {
+      SessionUser.getUser()
+      $state.go("groups.list");
+    }
+
+    function loginError(response) {
+      $scope.allertMsg = "User with curren login and password does not exist!";
+      $scope.isMsgHide = false;
+    }
+  }
+})();
